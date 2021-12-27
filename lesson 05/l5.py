deb5fbfa21e6798cac7e6ed85ce13fe9fd4a7e6a
xainse <xainse@users.noreply.github.com>
2021-12-27 18:22:32 +0200

    added class City and search + readme

--- lesson 05/l5.py
+++ lesson 05/l5.py
@@ -9,10 +9,6 @@ import sqlite3
 import csv
 
 
-
-# 1. Прочитати данні ітеративно із CSV файлу
-
-
 # 2. Створити базу даних, а якщо вона є - то очистити її 
 DB_NAME = 'ml-intern-testdb1.db'
 
@@ -38,35 +34,61 @@ else:
 cur.close()
 con.close()
 
-
-
-# 4. В циклі записати дані в базу даних. 
-
-# 5. 
-
-# 2. Створити базу даних, а якщо вона є - то очистити її
-
-
 file_path_csv = "./full_city_list.txt" # але насправді я знаю, що всередині данні у вигляді CSV
 
 con = sqlite3.connect(DB_NAME)
 cur = con.cursor()  # створюємо об'єкт-курсор
 
+# 1. Прочитати данні ітеративно із CSV файлу
 with open(file_path_csv, "r") as file_object:
     csv_file = csv.DictReader(file_object, delimiter=';', quotechar='"')
     
     sql = """\
-        INSERT INTO cities (country, city, CityId)
+        INSERT INTO cities (Country, City, CityId)
         VALUES (:Country, :City, :CityId)
     """
+    # 4. В циклі записати дані в базу даних.
     for row in csv_file:
         try:
             cur.execute(sql, row)
+            
         except sqlite3.DatabaseError as err:
             print("SQL error: ", err)
         else:
             con.commit()
-
 cur.close()
 con.close()
+
+class City:
+   
+    def search(self, query = ''):
+        con = sqlite3.connect(DB_NAME)
+        cur = con.cursor()  # створюємо об'єкт-курсор
+
+        query += '%'
+
+        sql = """\
+            SELECT Country, City, CityId FROM cities
+            WHERE City LIKE :query
+            """
+        
+        cur.execute(sql, {'query': query})
+        result = cur.fetchall()
+
+        cur.close()
+        con.close()
+
+        return result
+
+
+city = City()
+
+print('Search citie\'s code by name.')
+print('Write city name or first letters below:')
+query = input()
+
+cities = city.search(query)
+print ("Found cities in DB:")
+print(cities)
+        
 
